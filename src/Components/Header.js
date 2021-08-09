b59333842823f436604104c4da5c878f1fbb8ed3
ankit <ac2000choudhary@gmail.com>
2021-08-09 08:15:27 +0530

    Header Component added

---
+++ src/Components/Header.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export const Header = () => {
+  const[isActive, setIsActive] = useState(false);
+  return (
+    <nav
+      className="navbar has-background-info"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <Link className="navbar-item" to="/">
+          <h3 className="title">PayDoni</h3>
+        </Link>
+
+        <button
+          className={isActive ? "navbar-burger is-active" : "navbar-burger"}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navigation-bar"
+          onClick={() => setIsActive(!isActive)}
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </button>
+      </div>
+
+      <div id="navigation-bar" className={isActive ?"navbar-menu has-background-info-light is-active":"navbar-menu"} onClick={() => setIsActive(false)}>
+        <div className="navbar-start">
+          <Link className="navbar-item" to="/">
+            Home
+          </Link>
+
+          <Link className="navbar-item" to="/about">
+            About
+          </Link>          
+        </div>
+        <div className="navbar-end my-2 mr-4">
+        <Link className="navbar-item button is-primary is-light" to="/about">
+            Donate Now
+          </Link> 
+        </div>
+      </div>
+    </nav>
+  );
+};
+
