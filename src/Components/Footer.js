5bda5dae4a69d3e38f7a9ff7f5cbbfe64fa28415
ankit <ac2000choudhary@gmail.com>
2021-08-09 12:16:41 +0530

    Footer updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,12 +3,12 @@ import React from "react";
 export const Footer = () => {
   const footerStyle = {
     position: "relative",
-    top: "16vh",
+    top: "12vh",
     width: "100%",
   };
 
   return (
-    <footer className="footer has-background-dark" style={footerStyle}>
+    <footer className="footer has-background-dark mt-6" style={footerStyle}>
       <div className="content has-text-centered has-text-white">
         <p>
           <strong className="has-text-white">PayDoni</strong> created by{" "}
