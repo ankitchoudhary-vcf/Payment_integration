4ded886a64f351c6d50cfa204c277e26a0b8f46d
ankit <ac2000choudhary@gmail.com>
2021-08-09 12:16:23 +0530

    Home updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { About } from "./About";
+import { CarouselComponent } from "./CarouselComponent";
 
 export const Home = () => {
-    return (
-        <div>
-            Home!!
+  return (
+    <>
+      <div className="columns is-mobile is-centered is-hidden-desktop">
+        <div className="column is-6-desktop is-full-touch">
+          <CarouselComponent />
         </div>
-    )
-}
+      </div>
+
+      <div className="columns is-mobile is-centered is-hidden-touch mt-6">
+        <div className="column is-6-desktop is-full-touch">
+          <CarouselComponent />
+        </div>
+      </div>
+
+      <About />
+    </>
+  );
+};
